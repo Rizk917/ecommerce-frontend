eb1c8c3b31f56b93c84f277987988dd25a1648bd
achrafAR <achraf.alrachini@gmail.com>
2023-04-25 09:55:17 +0300

    fetching data add the update with few edit

--- src/Components/Products/product.js
+++ src/Components/Products/product.js
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import'./product.css'
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import "./product.css";
+import axios from "axios";
 
 function Product() {
-
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
-  const [searchTerm,setSearchTerm] = useState("");
-
-  
+  const [searchTerm, setSearchTerm] = useState("");
 
   // get products using axios
   useEffect(() => {
@@ -23,13 +19,12 @@ function Product() {
       });
   }, [products]);
 
-
   // get categories using axios
   useEffect(() => {
     axios
       .get("http://localhost:5000/categories")
       .then((response) => {
-        setCategories(response.data.data); 
+        setCategories(response.data.data);
       })
       .catch((error) => {
         console.log(error);
@@ -37,221 +32,286 @@ function Product() {
   }, [categories]);
 
   // add data to products
-  
-  const [addProduct,setAddProduct] = useState({
-    productName:"",
-    productDescription:"",
-    productImage:"",
-    productPrice:"",
-    productQuantity:"",
-    categoryId:""
-  })
-  
-// add product
+
+  const [addProduct, setAddProduct] = useState({
+    productName: "",
+    productDescription: "",
+    productImage: "",
+    productPrice: "",
+    productQuantity: "",
+    categoryId: "",
+  });
+
+  // add product
   const handleChange = async (event) => {
     event.preventDefault();
-    
-    const fieldName = event.target.getAttribute('name')
-  let fieldValue = event.target.value;
-  if(fieldName === 'image'){
-    fieldValue = event.target.files[0];
-}
 
+    const fieldName = event.target.getAttribute("name");
+    let fieldValue = event.target.value;
+    if (fieldName === "image") {
+      fieldValue = event.target.files[0];
+    }
 
-    const newFormData = { ...addProduct};
+    const newFormData = { ...addProduct };
     newFormData[fieldName] = fieldValue;
 
-    setAddProduct(newFormData)
-
-    
-  }
+    setAddProduct(newFormData);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const formData = new FormData();
-      formData.append('productName',addProduct.productName)
-      formData.append('productDescription',addProduct.productDescription)
-      formData.append('productImage',addProduct.productImage)
-      formData.append('productPrice',addProduct.productPrice)
-      formData.append('productQuantity',addProduct.productQuantity)
-      formData.append('categoryId',String(addProduct.categoryId))
-    
+    formData.append("productName", addProduct.productName);
+    formData.append("productDescription", addProduct.productDescription);
+    formData.append("productImage", addProduct.productImage);
+    formData.append("productPrice", addProduct.productPrice);
+    formData.append("productQuantity", addProduct.productQuantity);
+    formData.append("categoryId", String(addProduct.categoryId));
 
     const config = {
-      headers:{'content-type':"multipart/form-data"}
+      headers: { "content-type": "multipart/form-data" },
     };
-    
+
     axios
-    .post('http://localhost:5000/products', formData, config)
-    .then((response) => {
-      setProducts([...products, response.data])
-    })
-    .catch((error) => {
-      console.log(error.response.data)
-    })
+      .post("http://localhost:5000/products", formData, config)
+      .then((response) => {
+        setProducts([...products, response.data]);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
 
     handleShowProduct();
-
-  }
-  
-  
-
+  };
 
   // Function for deleting  a product
 
-  
   const handleDeleteProduct = async (id) => {
     const url = `http://localhost:5000/products/${id}`;
-    try{
+    try {
       await axios.delete(url);
-      setProducts(products.filter(product => product._id !== id));
-      console.log('Product deleted successfully!');
+      setProducts(products.filter((product) => product._id !== id));
+      console.log("Product deleted successfully!");
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-      console.log(error)
-    }
-    
   };
-
-
-
 
   // update products
 
-
-  const [currentProduct, setCurrentProduct] = useState('null')
+  const [currentProduct, setCurrentProduct] = useState("null");
   const [showProduct, setShowProduct] = useState(false);
   const [showUpdateForm, setShowUpdateForm] = useState(false);
 
-
-
-  const handleShowProduct = () =>{
-    setShowProduct(!showProduct)
-  }
+  const handleShowProduct = () => {
+    setShowProduct(!showProduct);
+  };
 
   return (
-    <div className='container'>
-      <div className='page_name'><h1>Products</h1></div>
-      <div className='table_container'>
-        <div className='search_table'>
-        <div className="search">
-        <input
-            placeholder="Search By Product Name"
-            onChange={(event) => {
-              setSearchTerm(event.target.value)
-            }}
-
-        />
-        </div>
-        <table className='table'>
-        <thead className='head_table'>
-          <tr className='table_head_tr'>
-            <th>Product Name</th>
-            <th>Update</th>
-            <th>Delete</th>
-
-
-          </tr>
-        </thead>
-        
-        <tbody className='table_tbody'>
-          {
-          products.filter((product) => {
-            if(!searchTerm){
-              return product
-            }else if (product.productName.toLowerCase().includes(searchTerm.toLowerCase())){
-              return product
-            }else{
-              return null
-            }
-          }).map((product, key) => {
-            return (
-              <tr  className='table_tr' key={key}>
-              <td className='table_td'>{product.productName}</td>
-              <td className='table_td'>
-                <button onClick={() => {
-                  setShowUpdateForm(!showUpdateForm);
-                  setCurrentProduct(product)
-
-                }}>
-                <img src={process.env.PUBLIC_URL + '/pictures/edit.png'} alt='edit' className='edit' />
-
-                </button>
-              </td>
-              <td className='table_td'>
-                <button onClick={() => handleDeleteProduct(product._id)}>
-                <img src={process.env.PUBLIC_URL + '/pictures/delete.png'} alt='delete' className='delete' />
-
-                </button>
-              </td>
-            </tr>
-            );
-          })}
-        
-            
-        </tbody>
-      </table>
-      {showUpdateForm && currentProduct?(
-        <div className='update_product'>
-      <form className='product_form' >
-            <label className='product_label'>Product Name</label>
-            <input   type="text" className="product" name="name" defaultValue={currentProduct.productName}/>
-            <label className='product_label'>Product Description</label>
-            <input   type="text" className="product" name="description" defaultValue={currentProduct.productDescription}/>
-            <label className='product_label'>Product Image</label>
-            <input   type="file" className="product" name="image" defaultValue={currentProduct.productImage.file}/>
-            <label className='product_label'>Product Price</label>
-            <input   type="text" className="product" name="price" defaultValue={currentProduct.productPrice}/>
-            <label className='product_label'>Product Quantity</label>
-            <input   type="text" className="product" name="quantity" defaultValue={currentProduct.productQuantity}/>
-            <select className='product' name='categoryId' onChange={handleChange}>
-            {categories.map((category, key) => (
-            <option className='option' key={key} value={category._id} defaultValue={category.categoryName}>{category.categoryName}</option>
-  ))}
-</select>
-            <button className='submit'>Submit</button>
-            
-          </form>
+    <div className="container">
+      <div className="page_name">
+        <h1>Products</h1>
       </div>
-      ) : null}
-      
+      <div className="table_container">
+        <div className="search_table">
+          <div className="search">
+            <input
+              placeholder="Search By Product Name"
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
+            />
+          </div>
+          <table className="table">
+            <thead className="head_table">
+              <tr className="table_head_tr">
+                <th>Product Name</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+
+            <tbody className="table_tbody">
+              {products
+                .filter((product) => {
+                  if (!searchTerm) {
+                    return product;
+                  } else if (
+                    product.productName
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase())
+                  ) {
+                    return product;
+                  } else {
+                    return null;
+                  }
+                })
+                .map((product, key) => {
+                  return (
+                    <tr className="table_tr" key={key}>
+                      <td className="table_td">{product.productName}</td>
+                      <td className="table_td">
+                        <button
+                          onClick={() => {
+                            setShowUpdateForm(!showUpdateForm);
+                            setCurrentProduct(product);
+                          }}
+                        >
+                          <img
+                            src={process.env.PUBLIC_URL + "/pictures/edit.png"}
+                            alt="edit"
+                            className="edit"
+                          />
+                        </button>
+                      </td>
+                      <td className="table_td">
+                        <button
+                          onClick={() => handleDeleteProduct(product._id)}
+                        >
+                          <img
+                            src={
+                              process.env.PUBLIC_URL + "/pictures/delete.png"
+                            }
+                            alt="delete"
+                            className="delete"
+                          />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+          {showUpdateForm && currentProduct ? (
+            <div className="update_product">
+              <form className="product_form">
+                <label className="product_label">Product Name</label>
+                <input
+                  type="text"
+                  className="product"
+                  name="name"
+                  defaultValue={currentProduct.productName}
+                />
+                <label className="product_label">Product Description</label>
+                <input
+                  type="text"
+                  className="product"
+                  name="description"
+                  defaultValue={currentProduct.productDescription}
+                />
+                <label className="product_label">Product Image</label>
+                <input
+                  type="file"
+                  className="product"
+                  name="image"
+                  defaultValue={currentProduct.productImage.file}
+                />
+                <label className="product_label">Product Price</label>
+                <input
+                  type="text"
+                  className="product"
+                  name="price"
+                  defaultValue={currentProduct.productPrice}
+                />
+                <label className="product_label">Product Quantity</label>
+                <input
+                  type="text"
+                  className="product"
+                  name="quantity"
+                  defaultValue={currentProduct.productQuantity}
+                />
+                <select
+                  className="product"
+                  name="categoryId"
+                  onChange={handleChange}
+                >
+                  {categories.map((category, key) => (
+                    <option
+                      className="option"
+                      key={key}
+                      value={category._id}
+                      defaultValue={category.categoryName}
+                    >
+                      {category.categoryName}
+                    </option>
+                  ))}
+                </select>
+                <button className="submit">Submit</button>
+              </form>
+            </div>
+          ) : null}
         </div>
-      
-      <div className='add_product'>
-      <button className='add_product' onClick={() => handleShowProduct()}>+ Add Product</button>
-      {showProduct ? (
-          <div className="add_product_form" >
-          <form  className='product_form'>
-            <label className='product_label'>Product Name</label>
-            <input   type="text" className="product" name="productName"  autoComplete='off' onChange={handleChange}/>
-            <label className='product_label'>Product Description</label>
-            <input   type="text" className="product" name="productDescription"   autoComplete='off' onChange={handleChange} />
-            <label className='product_label'>Product Image</label>
-            <input   type="file" className="product" name="productImage"  autoComplete='off' onChange={handleChange} />
-            <label className='product_label'>Product Price</label>
-            <input   type="text" className="product" name="productPrice"  autoComplete='off' onChange={handleChange} />
-            <label className='product_label'>Product Quantity</label>
-            <input   type="text" className="product" name="productQuantity" autoComplete='off' onChange={handleChange}/>
-            <label className='product_label'>Select Category</label>
-            <select className='product' name='categoryId' onChange={handleChange}>
-            {categories.map((category, key) => (
-            <option className='option' key={key} value={category._id}>{category.categoryName}</option>
-  ))}
-</select>
 
-            <button className='submit' onClick={handleSubmit}>Submit</button>
-        
-          </form>
-        </div> 
-      ): null}
-  
+        <div className="add_product">
+          <button className="add_product" onClick={() => handleShowProduct()}>
+            + Add Product
+          </button>
+          {showProduct ? (
+            <div className="add_product_form">
+              <form className="product_form">
+                <label className="product_label">Product Name</label>
+                <input
+                  type="text"
+                  className="product"
+                  name="productName"
+                  autoComplete="off"
+                  onChange={handleChange}
+                />
+                <label className="product_label">Product Description</label>
+                <input
+                  type="text"
+                  className="product"
+                  name="productDescription"
+                  autoComplete="off"
+                  onChange={handleChange}
+                />
+                <label className="product_label">Product Image</label>
+                <input
+                  type="file"
+                  className="product"
+                  name="productImage"
+                  autoComplete="off"
+                  onChange={handleChange}
+                />
+                <label className="product_label">Product Price</label>
+                <input
+                  type="text"
+                  className="product"
+                  name="productPrice"
+                  autoComplete="off"
+                  onChange={handleChange}
+                />
+                <label className="product_label">Product Quantity</label>
+                <input
+                  type="text"
+                  className="product"
+                  name="productQuantity"
+                  autoComplete="off"
+                  onChange={handleChange}
+                />
+                <label className="product_label">Select Category</label>
+                <select
+                  className="product"
+                  name="categoryId"
+                  onChange={handleChange}
+                >
+                  {categories.map((category, key) => (
+                    <option className="option" key={key} value={category._id}>
+                      {category.categoryName}
+                    </option>
+                  ))}
+                </select>
 
+                <button className="submit" onClick={handleSubmit}>
+                  Submit
+                </button>
+              </form>
+            </div>
+          ) : null}
+        </div>
       </div>
-    
     </div>
-    </div>
-    
   );
 }
 
